7438411434abdfa4da60a97203a41f3ea99101c9
Guenyoung <emsud@inu.ac.kr>
2020-01-27 23:55:54 +0900

    feature : add Spring Container to applictaionContext
    스프링 컨테이너를 통한 객체 생성을 확인하기 위해 applictaionContext.xml 파일에 bean 엘리먼트를 추가.
    해당 bean 엘리먼트는 init-method와 destroy-method를 가지고 있으며 AbastractApplicationContext를 이용하여 컨테이너를 구동 시킴.
    bean 엘리먼트에 scope를 따로 지정해주지 않았기에 기본 스코프는 singletone 객체를 가진다. 따라서 아무리 객체를 더 생성해도 처음 생성된 객체 이외에는 생성되지 않는 특성을 보장한다.

--- src/main/java/polymorphism/TVUser.java
+++ src/main/java/polymorphism/TVUser.java
@@ -1,14 +1,23 @@
 package polymorphism;
 
-public class TVUser {
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
+public class TVUser {
 	public static void main(String[] args) {
-		BeanFactory factory = new BeanFactory();
-		TV tv = (TV) factory.getBean(args[0]);
+		// 1. Spring 컨테이너를 구동.
+		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
+	
+		// 2. Spring 컨테이너로부터 필요한 객체를 요청 한다.
+		// applictionContext.xml에 bean으로 정의되 있는 것 중에서 id가 tv인 객체를 가져옴.
+		TV tv = (TV)factory.getBean("tv");
 		tv.powerOn();
 		tv.volumeUp();
 		tv.volumeDown();
 		tv.powerOff();
+		
+		// 3. Spring 컨테이너를 종료한다.
+		factory.close();
 	}
 
 }

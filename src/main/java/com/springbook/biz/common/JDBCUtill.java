c57aea3c1cd7d49564f2a42d25bd6780eaeed73e
Guenyoung <emsud@inu.ac.kr>
2020-01-30 16:31:29 +0900

    feature : add JDBC Utility class
    데이터베이스 연동을 위한 JDBC 유틸리티를 common package에 추가.
    해당 클래스는 DB와 connection 과 close를 처리한다.
    close 메소드는 오버로딩 하여 매개변수의 타입에 따라 드리게 처리하도록 구현. 첫번째 close 메소드는 PreparedStatement와 Connectio을 닫고 그 값을 초기화 시키도록 하였고
    두번 째 메소드는 이에 더하여 ResultSet에 값 역시 close와 초기화 처리를 수행하도록 함.

---
+++ src/main/java/com/springbook/biz/common/JDBCUtill.java
@@ -0,0 +1,88 @@
+package com.springbook.biz.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+public class JDBCUtill {
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			 // mysql은 "jdbc:mysql://localhost/사용할db이름" 이다.
+            String url = "jdbc:mysql://localhost/boardWeb";
+            
+            // user와 password는 깃허브에 올릴 시 바꿔서 올려야함.
+			return DriverManager.getConnection(url);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static void close(PreparedStatement stmt, Connection conn) {
+		if(stmt != null) {
+			try {
+				if(!stmt.isClosed()) {
+					stmt.close();
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}finally {
+				stmt = null;
+			}
+		}
+		
+		if(conn != null) {
+			try {
+				if(!conn.isClosed()) {
+					conn.close();
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}finally {
+				conn = null;
+			}
+		}
+	}
+	
+	public static void close(ResultSet rs, PreparedStatement stmt, Connection conn) {
+		if(rs != null) {
+			try {
+				if(!rs.isClosed()) {
+					rs.close();
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				rs = null;
+			}
+		}
+		
+		if(stmt != null) {
+			try {
+				if(!stmt.isClosed()) {
+					stmt.close();
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}finally {
+				stmt = null;
+			}
+		}
+		
+		if(conn != null) {
+			try {
+				if(!conn.isClosed()) {
+					conn.close();
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}finally {
+				conn = null;
+			}
+		}
+	}
+}

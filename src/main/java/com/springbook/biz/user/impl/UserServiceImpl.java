047003f1bc87c6c6326cbcfa8f5fcf89a2b5d3ab
Guenyoung <emsud@inu.ac.kr>
2020-01-30 19:55:39 +0900

    feature : add AOP
    AOP 설정을 위해 스프링 설정 파일을 수정 aop 포인트컷을 두개 추가.
    첫번째 포인트 컷은 biz 패키지 내에 있는 Impl로 끝나는 클래스의 모든 메소드를 포인트컷으로 설정.
    두번째 포인트 컷은 위와 동일하나 get으로 시작하는 메소드를 포인트컷으로 설정하도록 필터링함.
    해당 포인트컷을 사용하여 핵심관심에서 로그 메소드를 횡단관심으로 자바코드 수정없이 사용할 수 있는 이점이 있다.

--- src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
+++ src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package com.springbook.biz.user.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.springbook.biz.user.UserService;
 import com.springbook.biz.user.UserVO;
 
+@Service("userService")
 public class UserServiceImpl implements UserService {
 	private UserDAO userDAO;
 	

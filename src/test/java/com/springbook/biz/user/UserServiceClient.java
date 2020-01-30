13922ed13317df448e4e8aa0917fb7a60d4141ca
Guenyoung <emsud@inu.ac.kr>
2020-01-30 19:53:08 +0900

    refactor : move UserServiceClient into com.springbook.biz.user
    board 패키지에 같이 존재하던 클라이언트 프로그램을 user 패키지를 새로 만들어 둘을 분리시킴.

--- src/test/java/com/springbook/biz/user/UserServiceClient.java
+++ src/test/java/com/springbook/biz/user/UserServiceClient.java
@@ -1,4 +1,4 @@
-package com.springbook.biz.board;
+package com.springbook.biz.user;
 
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;

3b895d051f4aaa2bfc4696cd76d921231e12e441
Guenyoung <emsud@inu.ac.kr>
2020-01-31 15:13:32 +0900

    refactor : using annotation based aop
    기존에 스프링 설정 파일에 선언되어있던 aop 관련 엘리먼트 대신 어노테이션기반 aop 설정으로 리팩토링
    1. applicationContext.xml 파일에 aop autoproxy를 선언.
    2. 외부 포인트 컷 파일을 생성하여 포인트 컷에 대한 표현식과 뼈대 메소드를 정의한다.
    3. 어드바이스 메소드를 사용할 객체에 어노테이션으로 @Aspect와 @Service를 정의하고 어드바이스 메소드 호출 시점을 메소드 위에 어노테이션으로 설정한다.
    4. 이 때 returnObj를 사용하는 경우 pointcut과 returnning을 따로 명시한다.

--- src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
+++ src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
@@ -1,18 +1,29 @@
 package com.springbook.biz.common;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
+
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
 import com.springbook.biz.user.UserVO;
 
+
+@Service
+@Aspect
 public class AfterReturningAdvice {
+	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
+	public void allPointcut(){}
+	
+	@AfterReturning(pointcut="allPointcut()", returning="returnObj")
 	public void afterLog(JoinPoint jp, Object returnObj) {
 		String method = jp.getSignature().getName();
 		if(returnObj instanceof UserVO) {
 			UserVO user = (UserVO)returnObj;
 		}
 		
-//		System.out.println("[사후 처리] "+method+"() 메소드 리턴값 : "+returnObj.toString());
+		System.out.println("[사후 처리] "+method+"() 메소드 리턴값 : "+returnObj.toString());
 	}
 }
--- src/main/java/com/springbook/biz/common/LogAdvice.java
+++ src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,6 +1,15 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
+
+@Aspect
+@Service
 public class LogAdvice {
+
+	@Before("PointcutCommon.allPointcut()")
 	public void printLog() {
 		System.out.println("[공통 로그] 비지니스 로직 수행 전 동작");
 	}
